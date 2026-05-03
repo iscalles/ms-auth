96a0409d640f9f529fce70878c2581bf2301a40c
iscalles <is.calles@duocuc.cl>
2026-05-03 17:19:17 -0400

    se modifica gitignore

--- authService/src/main/java/ms_auth/authService/AuthServiceApplication.java
+++ authService/src/main/java/ms_auth/authService/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package ms_auth.authService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		run(AuthServiceApplication.class, args);
 	}
 
 }
