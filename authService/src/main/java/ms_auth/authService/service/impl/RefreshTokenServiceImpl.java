812b98c7fd0349d80a0206ee7e0349a04ffb4b4b
Isabel Veronica Calles Rojas <is.calles@duocuc.cl>
2026-05-04 19:33:48 -0400

    feature: se implementa controller y service de RefreshToken

--- authService/src/main/java/ms_auth/authService/service/impl/RefreshTokenServiceImpl.java
+++ authService/src/main/java/ms_auth/authService/service/impl/RefreshTokenServiceImpl.java
@@ -1,4 +1,59 @@
 package ms_auth.authService.service.impl;
 
-public class RefreshTokenServiceImpl {
+import ms_auth.authService.model.RefreshToken;
+import ms_auth.authService.repository.RefreshTokenRepository;
+import ms_auth.authService.service.RefreshTokenService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+    final private RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @Override
+    public List<RefreshToken> listarRefreshToken() {
+        return refreshTokenRepository.findAll();
+    }
+
+    @Override
+    public RefreshToken buscarRefreshTokenPorId(Long id) {
+        return refreshTokenRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public RefreshToken crearRefreshToken(RefreshToken refreshToken) {
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    @Override
+    public RefreshToken actualizarRefreshToken(RefreshToken refreshToken, Long id) {
+        RefreshToken refreshTokenExistente = refreshTokenRepository.findById(id).orElse(null);
+        if (refreshTokenExistente != null) {
+            refreshTokenExistente.setToken(refreshToken.getToken());
+            refreshTokenExistente.setRutUsuario(refreshToken.getRutUsuario());
+            refreshTokenExistente.setFechaExpiracion(refreshToken.getFechaExpiracion());
+            return refreshTokenRepository.save(refreshTokenExistente);
+        } else {
+            throw new RuntimeException("Token inexistente");
+        }
+    }
+
+    @Override
+    public void eliminarRefreshToken(Long id) {
+        RefreshToken refreshTokenExistente = refreshTokenRepository.findById(id).orElse(null);
+        if (refreshTokenExistente != null) {
+            refreshTokenRepository.delete(refreshTokenExistente);
+        } else {
+            throw new RuntimeException("Token no existente");
+        }
+    }
+
+
+
+
 }
